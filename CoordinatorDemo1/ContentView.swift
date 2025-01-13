4be9fa5176a29132104b5fc6b3fa99ed8f1ea261
Waleed Saad Jebreen <waleedjebreen@gmail.com>
2025-01-13 13:58:12 +0300

    Initial Commit

---
+++ CoordinatorDemo1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoordinatorDemo1
+//
+//  Created by Waleed Jebrin on 13/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CoordinatorDemo1/CoordinatorDemo1App.swift
@@ -0,0 +1,17 @@
+//
+//  CoordinatorDemo1App.swift
+//  CoordinatorDemo1
+//
+//  Created by Waleed Jebrin on 13/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct CoordinatorDemo1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
